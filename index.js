08deab0d2aa607ea163952d567dc3d51fa3439ee
Christine QE <christine.q.ernst@gmail.com>
2017-10-24 16:09:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ function theBeatlesPlay(arrMusicians, arrInstruments) {
   }
   return finalArr;
 }
-/* describe('Beatles Loops', function() {
-  it("returns an array of strings containing what instruments each musician plays", function() {
-    const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-    const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-
-    expect(theBeatlesPlay(musicians, instruments)).toEqual(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
-  });
-
+function johnLennonFacts(factsArr) {
+  var factsArrExclamation = [];
+  while (i < factsArr.length) {
+    factsArrExclamation.push(factsArr[i] + "!!!")
+  }
+  return factsArrExclamation;
+}
+/* 
   describe('johnLennonFacts', function(){
     it("returns an array of strings with exclamation points", function() {
       expect(johnLennonFacts([
